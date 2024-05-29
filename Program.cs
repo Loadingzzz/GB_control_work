35af216fc613468456e9a528639b756d7b48f4ce
Nikolaev Ivan <privetpokanet@gmail.com>
2024-05-29 09:41:02 +0300

    complete Program.cs

--- Program.cs
+++ Program.cs
@@ -4,3 +4,35 @@
 // выполнения алгоритма. При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.
 
 
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string[] stringArray = new string[7] { "Иван", "Геналий", "123", "22", "987871823", "Реальность", "Lite" };
+        string[] resultArray = new string[stringArray.Length];
+
+        void SecondArrayWithIF(string[] array1, string[] array2)
+        {
+            int count = 0;
+            for (int i = 0; i < array1.Length; i++)
+            {
+                if (array1[i].Length <= 3)
+                {
+                    array2[count] = array1[i];
+                    count++;
+                }
+            }
+        }
+
+        void PrintArray(string[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write($"{array[i]} ");
+            }
+            Console.WriteLine();
+        }
+        SecondArrayWithIF(stringArray, resultArray);
+        PrintArray(resultArray);
+    }
+}
